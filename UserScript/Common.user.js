7b83359ed9a1c3cb8942780581d231a13b98f14e
玄清介 <dupontjoy@163.com>
2014-12-28 10:56:56 +0800

    12.28

---
+++ UserScript/Common.user.js
@@ -0,0 +1,58 @@
+// ==UserScript==
+// @name           常用腳本
+// @author         Dupont
+// @description    常用腳本以Require形式運行
+// @create         2014-12-28
+// @lastmodified   2014-12-28
+// @version        0.0.0.1
+// @updatelog      ....
+// @namespace      https://github.com/dupontjoy/customization/tree/master/UserScript
+// →→→DZ论坛签到工具.user↓↓↓
+// @require https://raw.githubusercontent.com/dupontjoy/customization/master/UserScript/DZ%E8%AE%BA%E5%9D%9B%E7%AD%BE%E5%88%B0%E5%B7%A5%E5%85%B7.user.js
+// →→→Error Page Auto Refresh.user↓↓↓
+// @require https://raw.githubusercontent.com/dupontjoy/customization/master/UserScript/Error%20Page%20Auto%20Refresh.user.js
+// →→→Shortened URL Prefetcher.user↓↓↓
+// @require https://raw.githubusercontent.com/dupontjoy/customization/master/UserScript/Shortened%20URL%20Prefetcher.user.js
+// →→→True URL downloads 2(Ⅱ).user↓↓↓
+// @require https://raw.githubusercontent.com/dupontjoy/customization/master/UserScript/True%20URL%20downloads%202%28%E2%85%A1%29.user.js
+// →→→XunLei Yunbo Better Player.user↓↓↓
+// @require https://raw.githubusercontent.com/dupontjoy/customization/master/UserScript/XunLei%20Yunbo%20Better%20Player.user.js
+// →→→禁止打印弹窗 for USL.user↓↓↓
+// @require https://raw.githubusercontent.com/dupontjoy/customization/master/UserScript/%E7%A6%81%E6%AD%A2%E6%89%93%E5%8D%B0%E5%BC%B9%E7%AA%97%20for%20USL.user.js
+// →→→自定义右键解锁.user↓↓↓
+// @require https://raw.githubusercontent.com/dupontjoy/customization/master/UserScript/%E8%87%AA%E5%AE%9A%E4%B9%89%E5%8F%B3%E9%94%AE%E8%A7%A3%E9%94%81.user.js
+// →→→去除CL1024需要插件提醒↓↓↓
+// @require https://raw.githubusercontent.com/dupontjoy/customization/master/UserScript/%E5%8E%BB%E9%99%A4CL1024%E9%9C%80%E8%A6%81%E6%8F%92%E4%BB%B6%E6%8F%90%E9%86%92.user.js
+
+// →→→stopGoogleRedirectionMod↓↓↓
+// @require https://raw.githubusercontent.com/ywzhaiqi/userscript/master/stopGoogleRedirectionMod@NLF.user.js
+// →→→修正 Discuz 论坛 relatedlink 的 bug↓↓↓
+// @require https://raw.githubusercontent.com/ywzhaiqi/userscript/master/bbs_discuz_relatedlink.user.js
+// →→→picviewer CE↓↓↓
+// @require https://raw.githubusercontent.com/ywzhaiqi/userscript/master/picviewerCE/picviewerCE.user.js
+
+// →→→百度网盘助手↓↓↓
+// @require https://greasyfork.org/scripts/986-%E7%99%BE%E5%BA%A6%E7%BD%91%E7%9B%98%E5%8A%A9%E6%89%8B/code/%E7%99%BE%E5%BA%A6%E7%BD%91%E7%9B%98%E5%8A%A9%E6%89%8B.user.js
+// →→→跳过网站等待、验证码及登录↓↓↓
+// @require https://greasyfork.org/scripts/2600-%E8%B7%B3%E8%BF%87%E7%BD%91%E7%AB%99%E7%AD%89%E5%BE%85-%E9%AA%8C%E8%AF%81%E7%A0%81%E5%8F%8A%E7%99%BB%E5%BD%95/code/%E8%B7%B3%E8%BF%87%E7%BD%91%E7%AB%99%E7%AD%89%E5%BE%85%E3%80%81%E9%AA%8C%E8%AF%81%E7%A0%81%E5%8F%8A%E7%99%BB%E5%BD%95.user.js
+// →→→AdsBypasser↓↓↓
+// @require https://greasyfork.org/scripts/4881-adsbypasser/code/AdsBypasser.user.js
+// →→→百度云插件+APIKey↓↓↓
+// @require https://greasyfork.org/scripts/496-%E7%99%BE%E5%BA%A6%E4%BA%91%E6%8F%92%E4%BB%B6-apikey/code/%E7%99%BE%E5%BA%A6%E4%BA%91%E6%8F%92%E4%BB%B6+APIKey.user.js
+// →→→YouTube Auto Buffer & Auto HD↓↓↓
+// @require https://greasyfork.org/scripts/494-youtube-auto-buffer-auto-hd/code/YouTube%20Auto%20Buffer%20%20Auto%20HD.user.js
+// →→→Super_preloaderPlus_one↓↓↓
+// @require https://greasyfork.org/scripts/293-super-preloaderplus-one/code/Super_preloaderPlus_one.user.js
+// →→→nolazyload↓↓↓
+// @require https://greasyfork.org/scripts/791-nolazyload/code/nolazyload.user.js
+// →→→百度貼吧繁體轉碼↓↓↓
+// @require https://greasyfork.org/scripts/4450-%E7%99%BE%E5%BA%A6%E8%B2%BC%E5%90%A7%E7%B9%81%E9%AB%94%E8%BD%89%E7%A2%BC/code/%E7%99%BE%E5%BA%A6%E8%B2%BC%E5%90%A7%E7%B9%81%E9%AB%94%E8%BD%89%E7%A2%BC.user.js
+// →→→Select text inside a link like Opera↓↓↓
+// @require https://greasyfork.org/scripts/789-select-text-inside-a-link-like-opera/code/Select%20text%20inside%20a%20link%20like%20Opera.user.js
+
+// @grant none
+
+// @included *
+
+// ==/UserScript==
+
